7d9003f538b08c659c1bf7195ad4f6fb17a3ef63
Tom Runnels <tjrunnels@liberty.edu>
2020-12-29 17:18:38 -0500

    changed app back to app

--- App.js
+++ App.js
@@ -24,7 +24,6 @@ import config from './aws-exports'
 Amplify.configure(config)
 import { withAuthenticator, S3Image } from 'aws-amplify-react-native'; 
 import { render } from 'react-dom';
-import ProjectorUI from './ProjectorUI';
 
 const signUpConfig = {
   hideAllDefaults: true,
@@ -342,7 +341,7 @@ function App() {
 
 
 //tomdo: change
-export default ProjectorUI //withAuthenticator(App, true);
+export default App //withAuthenticator(App, true);
 
 
 
